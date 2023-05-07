5d3b89e31562823c3f58662a67b30f356774f877
Jackson-da-Walrus <132394182+Jackson-da-Walrus@users.noreply.github.com>
2023-05-06 23:30:42 -0500

    3 files modified

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import './style.css';
 let diamonds = 0
 let clk_power = 1
 let price_ext = 1
+let w_ext = 1
+let s_ext = 1
+let c_ext = 1
+let u_ext = 1
 let timer = 0
 let dps = 0
 let btn_x = 0
@@ -21,41 +25,54 @@ gamestart();
 function gamestart(){
   diamond_btn.addEventListener("click", add);
   shop_btn.addEventListener("click", show);
-  
-
-
-  //document.getElementById('diamond_cursor').addEventListener('mousemove', (e) => {
+  W_pick.addEventListener("click", buy_w);
+    //document.getElementById('diamond_cursor').addEventListener('mousemove', (e) => {
     //document.getElementById('diamond_cursor').style.left = e.clientX;
   //});
 }
-
-
-
-
 function add(){
   diamonds+= clk_power
   statusText.textContent = `Current Diamonds ${diamonds}`;
   add = false
-  
-
-
-  
-}
-
-
-
-function buy_help(){
-  if(diamonds >= 10 * price_ext){
-    diamonds-= 10 * price_ext
-    statusText.textContent = `Current Diamonds ${diamonds}`;
-    clk_power+=1
-    price_ext+=.50
-    buy_help = false
-    
-  }
 }
 function show(){
-  document.getElementById('sidebar').classList.toggle('active');
+  document.getElementById('Tool_bar').classList.toggle('active');
+}
+function buy_w(){
+  if(diamonds >= 10 * w_ext){
+    diamonds-= 10 * w_ext
+    statusText.textContent = `Current Diamonds ${diamonds}`;
+    clk_power+=1
+    w_ext+= w_ext * .50
+    buy_w = false
+  }
+}
+function buy_s(){
+  if(diamonds >= 150 * s_ext){
+    diamonds-= 150 * s_ext
+    statusText.textContent = `Current Diamonds ${diamonds}`;
+    clk_power+=3
+    s_ext+= s_ext * .50
+    buy_s = false
+  }
+}
+function buy_c(){
+  if(diamonds >= 1000 * c_ext){
+    diamonds-= 1000 * c_ext
+    statusText.textContent = `Current Diamonds ${diamonds}`;
+    clk_power+=10
+    c_ext+= c_ext * .50
+    buy_c = false
+  }
+}
+function buy_u(){
+  if(diamonds >= 2500 * u_ext){
+    diamonds-= 2500 * u_ext
+    statusText.textContent = `Current Diamonds ${diamonds}`;
+    clk_power+=20
+    u_ext+= u_ext * .50
+    buy_u = false
+  }
 }
 
 
