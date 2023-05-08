011e3d21b78047a9f3154949b6a1298b2177291c
Jackson-da-Walrus <132394182+Jackson-da-Walrus@users.noreply.github.com>
2023-05-08 10:52:14 -0500

    2 files modified

--- index.js
+++ index.js
@@ -13,19 +13,8 @@ var drn_ext = 1
 var prnt_ext = 1
 var dps = 0
 
-
-
-
-
-
-
-
-
 statusText.textContent = `Current Diamonds ${diamonds}`;
 gamestart();
-
-
-
 
 function gamestart(){
   diamond_btn.addEventListener("click", add);
@@ -41,18 +30,6 @@ function gamestart(){
   Diamond_pnt.addEventListener("click", buy_prnt);
   
 }
-
-
-
-
-
-
-
-
-
-
-
-
 function add(){
   diamonds+= clk_power
   statusText.textContent = `Current Diamonds ${diamonds}`;
@@ -69,7 +46,7 @@ function buy_w(){
     diamonds-= 10 * w_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
     clk_power+=1
-    w_ext+= w_ext * .50
+    w_ext+= w_ext * .20
     buy_w = false
     diamonds = parseInt(diamonds)
   }
@@ -79,7 +56,7 @@ function buy_s(){
     diamonds-= 150 * s_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
     clk_power+=3
-    s_ext+= s_ext * .50
+    s_ext+= s_ext * .20
     buy_s = false
     diamonds = parseInt(diamonds)
   }
@@ -89,7 +66,7 @@ function buy_c(){
     diamonds-= 1000 * c_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
     clk_power+=10
-    c_ext+= c_ext * .50
+    c_ext+= c_ext * .20
     buy_c = false
     diamonds = parseInt(diamonds)
   }
@@ -99,47 +76,47 @@ function buy_u(){
     diamonds-= 2500 * u_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
     clk_power+=20
-    u_ext+= u_ext * .50
+    u_ext+= u_ext * .20
     buy_u = false
     diamonds = parseInt(diamonds)
   }
 }
 function buy_geo(){
-  if(diamonds >= 10 * geo_ext){
-    diamonds-= 10 * geo_ext
+  if(diamonds >= 250 * geo_ext){
+    diamonds-= 250 * geo_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
-    dps = dps + 1
-    geo_ext+= geo_ext * .50
+    clk_power += 1
+    geo_ext+= geo_ext * .40
     buy_geo = false
     diamonds = parseInt(diamonds)
   }
 }
 function buy_drl(){
-  if(diamonds >= 150 * drl_ext){
-    diamonds-= 150 * drl_ext
+  if(diamonds >= 1500 * drl_ext){
+    diamonds-= 1500 * drl_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
-    dps += 5
-    drl_ext+= drl_ext * .50
+    clk_power += 5
+    drl_ext+= drl_ext * .40
     buy_drl = false
     diamonds = parseInt(diamonds)
   }
 }
 function buy_drn(){
-  if(diamonds >= 1000 * drn_ext){
-    diamonds-= 1000 * drn_ext
+  if(diamonds >= 7500 * drn_ext){
+    diamonds-= 7500 * drn_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
-    
-    drn_ext+= drn_ext * .50
+    clk_power += 15
+    drn_ext+= drn_ext * .40
     buy_drn = false
     diamonds = parseInt(diamonds)
   }
 }
 function buy_prnt(){
-  if(diamonds >= 2500 * prnt_ext){
-    diamonds-= 2500 * prnt_ext
+  if(diamonds >= 15000 * prnt_ext){
+    diamonds-= 15000 * prnt_ext
     statusText.textContent = `Current Diamonds ${diamonds}`;
-    
-    prnt_ext+= prnt_ext * .50
+    clk_power += 30
+    prnt_ext+= prnt_ext * .40
     buy_prnt = false
     diamonds = parseInt(diamonds)
   }
